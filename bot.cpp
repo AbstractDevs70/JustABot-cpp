82501cfc391805f68ec4921ef676d0e21f8fbc12
AbstractDevs70 <sergreminder@gmail.com>
2024-06-21 15:52:28 +0300

    j

--- bot.cpp
+++ bot.cpp
@@ -4,19 +4,22 @@ dpp::cluster bot(BOT_TOKEN);
 
 int main() {
     bot.on_log(dpp::utility::cout_logger());
+    cout << dpp::colors::green;
  
     bot.on_slashcommand([](const dpp::slashcommand_t& event) {
         if (event.command.get_command_name() == "echo") {
             dpp::snowflake channel = event.command.get_channel().id;
+            cout << "echo | ";
             if (bancheck(event.command.get_issuing_user().id)==true){
                 std::string testovo = std::get<std::string>(event.get_parameter("text"));
                 bot.message_create(dpp::message(channel, testovo));
-                cout << "Команда сработала! " << testovo << " | " << event.command.get_issuing_user().global_name << " (" << event.command.get_issuing_user().username << ", " << event.command.get_issuing_user().id << ")" << endl;
                 event.thinking(true);
                 event.delete_original_response();
+                cout << testovo;
             }else {
                 event.reply(dpp::message("[ ! ] Вы заблокированы за нарушения"));
             }
+            cout << " | " << event.command.get_issuing_user().username << endl; 
         }
 
         if (event.command.get_command_name() == "random"){
@@ -24,6 +27,7 @@ int main() {
             double to = get<double>(event.get_parameter("до"));
             long int cfrom = from;
             long int cto = to;
+            cout << "random | ";
             switch (bancheck(event.command.get_issuing_user().id)){
                 case true:
                     event.thinking();
@@ -33,17 +37,22 @@ int main() {
                     long int random = rand() % raznica ;
                     long int randomi = from + random;
                     event.edit_original_response("Вам выпало:" + to_string( randomi ));
+                    cout << to_string(from) << "/" << to_string(to);
                     } else{
                         event.edit_original_response(dpp::message("Число от должно быть < числа до и ни одно число не равно 0!"));
+                        cout << "Заблокировано";
                     } break;
                 
                 case false:
                     event.reply(dpp::message("[ ! ] Вы заблокированы за нарушения"));
+                    cout << "Заблокировано";
                     break;
             }
+            cout << " | " << event.command.get_issuing_user().username << endl; 
          }
 
          if (event.command.get_command_name()=="calculate"){
+            cout << "calculate | ";
             event.thinking();
             dpp::snowflake channel = event.command.get_channel().id;
             string primer = get<string>(event.get_parameter("пример"));
@@ -60,7 +69,8 @@ int main() {
             switch(bancheck(event.command.get_issuing_user().id)){
                 case true:
                     if (answer != "" && answer.length() <= 2000){
-                        event.edit_original_response(answer);
+                        event.edit_original_response(primer + "=" + answer);
+                        cout << primer;
                     }else{
                         if(answer.length()>2000){
                             dpp::message err2(channel, "[ ! ] Ответ слишком длинный (>2000 символов)");
@@ -75,12 +85,15 @@ int main() {
                     break;
                 case false:
                     event.edit_original_response(dpp::message("[ ! ] Вы заблокированы за нарушения"));
+                    cout << "Заблокировано";
                     break;
             }
+            cout << " | " << event.command.get_issuing_user().username << endl; 
          }
 
          if(event.command.get_command_name()=="info"){
             event.reply("> AbstractBot\nСоздатель: AbstractDevs\n\n> Простой бот, написанный на C++ с использованием библиотеки D++\n\n> GitHub: \n> https://github.com/AbstractDevs70/JustABot-cpp");
+            cout << "info | | " << event.command.get_issuing_user().username; 
          }
 
          if(event.command.get_command_name()=="about"){
@@ -120,6 +133,119 @@ int main() {
                     );
                 }
             event.reply(emb);
+            cout << "about | "<< usr.username << " | " << event.command.get_issuing_user().username << endl; 
+         }
+
+         if (event.command.get_command_name()=="embed"){
+            cout << "embed | ";
+            event.thinking(true);
+            dpp::snowflake channel = event.command.get_channel().id;
+            dpp::embed cemb = dpp::embed();
+            if (event.get_parameter("цвет").index() != 0){
+                uint32_t gget = get<long int>(event.get_parameter("цвет"));
+                cemb.set_color(gget);
+                cout << to_string(gget) << ", ";
+            }
+            if (event.get_parameter("заголовок").index() != 0){
+                string gget = get<string>(event.get_parameter("заголовок"));
+                cemb.set_title(gget);
+                cout << gget << ", ";
+            }
+            if (event.get_parameter("описание").index() != 0){
+                string gget = get<string>(event.get_parameter("описание"));
+                cemb.set_description(gget);
+                cout << gget << ", ";
+            }
+            if (event.get_parameter("изображение").index() != 0){
+                string gget = get<string>(event.get_parameter("изображение"));
+                cemb.set_image(gget);
+                cout << gget << ", ";
+            }
+            if (event.get_parameter("картинка").index() != 0){
+                string gget = get<string>(event.get_parameter("картинка"));
+                cemb.set_thumbnail(gget);
+                cout << gget << ", ";
+            }
+            if (event.get_parameter("ссылка").index() != 0){
+                string gget = get<string>(event.get_parameter("ссылка"));
+                cemb.set_url(gget);
+                cout << gget << ", ";
+            }
+            if (event.get_parameter("футер-текст").index() != 0 && event.get_parameter("футер-иконка").index() != 0 ){
+                string gget1 = get<string>(event.get_parameter("футер-текст"));
+                string gget2 = get<string>(event.get_parameter("футер-иконка"));
+                cemb.set_footer(
+                    dpp::embed_footer()
+                    .set_text(gget1)
+                    .set_icon(gget2)
+                );
+                cout << gget1 << ", " << gget2 << ", ";
+            }
+            if (event.get_parameter("филд-заголовок").index() != 0 && event.get_parameter("филд-описание").index() != 0 && event.get_parameter("филд-инлайн").index() != 0){
+                string gget1 = get<string>(event.get_parameter("филд-заголовок"));
+                string gget2 = get<string>(event.get_parameter("филд-описание"));
+                bool gget3 = get<bool>(event.get_parameter("филд-инлайн"));
+                cemb.add_field(
+                    gget1,
+                    gget2,
+                    gget3
+                );
+                cout << gget1 << ", " << gget2 << ", ";
+            }
+
+            if (event.get_parameter("филд2-заголовок").index() != 0 && event.get_parameter("филд2-описание").index() != 0 && event.get_parameter("филд2-инлайн").index() != 0){
+                string gget1 = get<string>(event.get_parameter("филд2-заголовок"));
+                string gget2 = get<string>(event.get_parameter("филд2-описание"));
+                bool gget3 = get<bool>(event.get_parameter("филд2-инлайн"));
+                cemb.add_field(
+                    gget1,
+                    gget2,
+                    gget3
+                );
+                cout << gget1 << ", " << gget2 << ", ";
+            }
+
+            if (event.get_parameter("филд3-заголовок").index() != 0 && event.get_parameter("филд3-описание").index() != 0 && event.get_parameter("филд3-инлайн").index() != 0){
+                string gget1 = get<string>(event.get_parameter("филд3-заголовок"));
+                string gget2 = get<string>(event.get_parameter("филд3-описание"));
+                bool gget3 = get<bool>(event.get_parameter("филд3-инлайн"));
+                cemb.add_field(
+                    gget1,
+                    gget2,
+                    gget3
+                );
+                cout << gget1 << ", " << gget2 << ", ";
+            }
+
+            if (event.get_parameter("филд4-заголовок").index() != 0 && event.get_parameter("филд4-описание").index() != 0 && event.get_parameter("филд4-инлайн").index() != 0){
+                string gget1 = get<string>(event.get_parameter("филд4-заголовок"));
+                string gget2 = get<string>(event.get_parameter("филд4-описание"));
+                bool gget3 = get<bool>(event.get_parameter("филд4-инлайн"));
+                cemb.add_field(
+                    gget1,
+                    gget2,
+                    gget3
+                );
+                cout << gget1 << ", " << gget2 << ", ";
+            }
+
+            if (event.get_parameter("филд5-заголовок").index() != 0 && event.get_parameter("филд5-описание").index() != 0 && event.get_parameter("филд5-инлайн").index() != 0){
+                string gget1 = get<string>(event.get_parameter("филд5-заголовок"));
+                string gget2 = get<string>(event.get_parameter("филд5-описание"));
+                bool gget3 = get<bool>(event.get_parameter("филд5-инлайн"));
+                cemb.add_field(
+                    gget1,
+                    gget2,
+                    gget3
+                );
+                cout << gget1 << ", " << gget2  << ", ";
+            }
+
+            dpp::message msg(channel, cemb);
+            bot.message_create(msg);
+
+            event.delete_original_response();
+            cout << " | " << event.command.get_issuing_user().username << endl; 
          }
 
     });
@@ -151,12 +277,85 @@ int main() {
                 dpp::command_option(dpp::co_user, "пользователь", "...", true)
             );
 
+            dpp::slashcommand embed ("embed", "Создать эмбед", bot.me.id);
+            embed.add_option(
+                dpp::command_option(dpp::co_integer, "цвет", "...", false)
+            );
+            embed.add_option(
+                dpp::command_option(dpp::co_string, "заголовок", "...", false)
+            );
+            embed.add_option(
+                dpp::command_option(dpp::co_string, "описание", "...", false)
+            );
+            embed.add_option(
+                dpp::command_option(dpp::co_string, "изображение", "...", false)
+            );
+            embed.add_option(
+                dpp::command_option(dpp::co_string, "картинка", "...", false)
+            );
+            embed.add_option(
+                dpp::command_option(dpp::co_string, "ссылка", "...", false)
+            );
+            embed.add_option(
+                dpp::command_option(dpp::co_string, "филд-заголовок", "...", false)
+            );
+            embed.add_option(
+                dpp::command_option(dpp::co_string, "филд-описание", "...", false)
+            );
+            embed.add_option(
+                dpp::command_option(dpp::co_boolean, "филд-инлайн", "...", false)
+            );
+            embed.add_option(
+                dpp::command_option(dpp::co_string, "филд2-заголовок", "...", false)
+            );
+            embed.add_option(
+                dpp::command_option(dpp::co_string, "филд2-описание", "...", false)
+            );
+            embed.add_option(
+                dpp::command_option(dpp::co_boolean, "филд2-инлайн", "...", false)
+            );
+            embed.add_option(
+                dpp::command_option(dpp::co_string, "филд3-заголовок", "...", false)
+            );
+            embed.add_option(
+                dpp::command_option(dpp::co_string, "филд3-описание", "...", false)
+            );
+            embed.add_option(
+                dpp::command_option(dpp::co_boolean, "филд3-инлайн", "...", false)
+            );
+            embed.add_option(
+                dpp::command_option(dpp::co_string, "филд4-заголовок", "...", false)
+            );
+            embed.add_option(
+                dpp::command_option(dpp::co_string, "филд4-описание", "...", false)
+            );
+            embed.add_option(
+                dpp::command_option(dpp::co_boolean, "филд4-инлайн", "...", false)
+            );
+            embed.add_option(
+                dpp::command_option(dpp::co_string, "филд5-заголовок", "...", false)
+            );
+            embed.add_option(
+                dpp::command_option(dpp::co_string, "филд5-описание", "...", false)
+            );
+            embed.add_option(
+                dpp::command_option(dpp::co_boolean, "филд5-инлайн", "...", false)
+            );
+            embed.add_option(
+                dpp::command_option(dpp::co_string, "футер-текст", "...", false)
+            );
+            embed.add_option(
+                dpp::command_option(dpp::co_string, "футер-иконка", "...", false)
+            );
+            
+
             
             bot.global_command_create(echo);
             bot.global_command_create(random);
             bot.global_command_create(calc);
             bot.global_command_create(info);
             bot.global_command_create(about);
+            bot.global_command_create(embed);
             bot.set_presence(dpp::presence(dpp::ps_online, dpp::at_custom, "У меня появился гитхаб (используй /info)"));
         }
     });
---
+++ test.sh
@@ -0,0 +1,1 @@
+g++ -std=c++17 bot.cpp -o host -ldpp && ./host
